7b108157cd1b9309eaed4f6cacaa043378302155
houra49 <houra.shamshyrcar@gmail.com>
2021-01-17 17:41:15 +0100

    houra49/Lev3_3_Project_css-einf-hrung_Illustration

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+function darkMode() {
+    document.body.style.backgroundColor = "#000000"
+    document.getElementById("anchor").style.color = '#ffff'
+    document.getElementById("anchor1").style.color = '#ffff'
+    document.getElementById("anchor2").style.color = '#ffff'
+    document.getElementById("absord").style.color = '#ffff'
+    document.getElementById("what").style.color = '#ffff'
+    document.getElementById("beauty").style.color = '#ffff'
+    document.getElementById("in").style.color = '#ffff'
+    document.getElementById("new").style.color = '#ffff'
+    document.getElementById("deep").style.color = '#ffff'
+    document.getElementById("ill").style.color = '#ffff'
+    document.getElementById("space").style.color = '#ffff'
+    document.getElementById("use").style.color = '#ffff'
+    document.getElementById("each").style.color = '#ffff'
+    document.getElementById("free").style.color = '#ffff'
+    document.getElementById("give").style.color = '#ffff'
+    document.getElementById("box").style.backgroundColor = "#172441"
+    document.getElementById("mem").style.color = '#ffff'
+    document.getElementById("num").style.color = '#ffff'
+    document.getElementById("link").style.color = '#ffff'
+    document.getElementById("any").style.color = '#ffff'
+    document.getElementById("super").style.color = '#aca9a9'
+    document.getElementById("list").style.color = '#aca9a9'
+    document.getElementById("email").style.color = '#ffff'
+    document.getElementById("follow").style.color = '#ffff'
+    document.getElementById('img1').src = '/assets/img/darkmode-img4.png'
+    document.getElementById('img2').src = '/assets/img/darkmode-img5.png'
+    document.getElementById('img3').src = '/assets/img/darkmode-img6.png'
+    var element = document.body;
+    element.classList.toggle("darkMode")
+}
+
+
+
